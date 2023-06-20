1c9b2011b4cf3c7d1278c60ce71d8f4e25441fda
TomasSanGiovanni <tomassangiovanni1@gmail.com>
2023-06-20 00:07:04 -0300

    Modifico php

--- php/Formulario.php
+++ php/Formulario.php
@@ -1,33 +1,11 @@
-<?php
-$nombre = $_POST('Nombre');
-$apellido = $_POST('Apellido');
-$Dni = $_POST('Dni');
-$email = $_POST('email');
+<?php 
+	$nombre = $_POST['Nombre'];
+	$email = $_POST['email'];
+	$asunto = 'Formulario Rellenado';
+	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['mensaje'];
 
-$destinatario = "tomassangiovanni1@gmail.com";
-$asunto = "Inscripcion evento 26 de junio realizada con exito";
 
-$cuerpo = '
-    <html>
-    <head>
-        <title>Inscripción</title>
-    </head>
-    <body>
-        <h1>Inscripcion evento nuevas tecnologias</h1>
-        <p>
-            Contacto: '.$nombre . ' - '.$apellido .' '.$Dni .' <br>
-            Mensaje: '.$asunto.'
-
-        </p>
-    </body>
-    </html>
-  ';
- $headers = "MIME-Version: 1.0\r\n";
- $headers .= "Content-type: text/html; charset=UTF8\r\n";
-
-$headers .= "FROM: $nombre <$email> \r\n";
-
-mail($destinatario,$asunto,$cuerpo,$headers);
-echo"correo enviado";
-
-?>
+	if(mail('Tomassangiovanni1@gmail.com', $asunto, $mensaje)){
+		echo "Correo enviado";
+	}
+ ?>
